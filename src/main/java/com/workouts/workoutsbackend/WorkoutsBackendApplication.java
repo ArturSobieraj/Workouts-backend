c7bdbd64861c29ccbc41077ec2b3f44b45828ce3
ArturSobieraj <artur.sobieraj126@gmail.com>
2019-11-30 21:27:43 +0100

    first commit

---
+++ src/main/java/com/workouts/workoutsbackend/WorkoutsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.workouts.workoutsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workouts/workoutsbackend/WorkoutsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workouts.workoutsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
